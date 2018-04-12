422aaf2bf5ff26f866950bfd58cc1bf9b02187e6
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-12 23:31:25 +0700

    pf-java test nextday

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,22 +1,23 @@
 public class NextDayCalculator {
-    public static boolean checkyear(int year){
-        boolean checkyear =true;
-        if(year % 4 == 0){
-            if(year % 100 == 0){
-                if(year % 400 == 0){
-                    return checkyear ==true;
+    public static boolean checkyear(int year) {
+        boolean checkyear = true;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return checkyear == true;
                 } else {
-                    return checkyear==false;
+                    return checkyear == false;
                 }
             } else {
-                return checkyear==false;
+                return checkyear == false;
             }
         } else {
-            return checkyear==false;
+            return checkyear == false;
         }
     }
-    public static String nextDay(int day , int month, int year){
-        int nextDay= day+1;
+
+    public static String nextDay(int day, int month, int year) {
+        int nextDay = day + 1;
         switch (month) {
             // Các tháng 1, 3, 5, 7, 8, 10 và 12 có 31 ngày.
             case 1:
@@ -25,36 +26,36 @@ public class NextDayCalculator {
             case 7:
             case 8:
             case 10:
-                if(nextDay>31){
-                    nextDay=1;
+                if (nextDay > 31) {
+                    nextDay = 1;
                     month++;
                 }
                 break;
-            // Các tháng 4, 6, 9 và 11 có 30 ngày
+            // Các tháng 4, 6, 9 và 11 có 30 ngày.
             case 4:
             case 6:
             case 9:
             case 11:
-                if(nextDay>30){
-                    nextDay=1;
+                if (nextDay > 30) {
+                    nextDay = 1;
                     month++;
                 }
                 break;
-             case 12:
-            if(nextDay>31){
-                nextDay=1;
-                month=1;
-                year++;
-            }
-            break;
-            //  Tháng 2 nếu là năm nhuận thì có 29 ngày, còn không thì có 28 ngày.
+            case 12:
+                if (nextDay > 31) {
+                    nextDay = 1;
+                    month = 1;
+                    year++;
+                }
+                break;
+            //  Tháng 2 nếu là năm nhuận thì có 29 ngày , còn không thì có 28 ngày.
             case 2:
-                if(checkyear(year) ==true &&nextDay>29){
-                    nextDay=1;
+                if (checkyear(year) == true && nextDay > 29) {
+                    nextDay = 1;
                     month++;
                 }
-                if (checkyear(year)==false && nextDay>28){
-                    nextDay=1;
+                if (checkyear(year) == false && nextDay > 28) {
+                    nextDay = 1;
                     month++;
                 }
 
@@ -62,7 +63,7 @@ public class NextDayCalculator {
             default:
         }
 
-        String date = nextDay+"/"+month+"/"+year;
+        String date = nextDay + "/" + month + "/" + year;
         return date;
     }
 }
